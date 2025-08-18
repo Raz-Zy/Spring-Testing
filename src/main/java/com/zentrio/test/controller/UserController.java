898e26f09e7fc3b9e1979f39132478f01cb9c725
Tan Dara <140366248+Raz-Zy@users.noreply.github.com>
2025-08-18 12:06:02 +0700

    Update UserController.java

--- src/main/java/com/zentrio/test/controller/UserController.java
+++ src/main/java/com/zentrio/test/controller/UserController.java
@@ -4,6 +4,7 @@ package com.zentrio.test.controller;
 import com.zentrio.test.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Operation;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
